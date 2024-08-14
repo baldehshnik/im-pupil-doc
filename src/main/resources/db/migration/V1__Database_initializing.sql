96cf7f72904cfc514bcca52d58caf40e08aa360a
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-08-14 21:48:39 +0300

    create migrations for database initializing

---
+++ src/main/resources/db/migration/V1__Database_initializing.sql
@@ -0,0 +1,438 @@
+CREATE TABLE educational_intitution (
+                                        id integer PRIMARY KEY AUTO_INCREMENT,
+                                        name text UNIQUE NOT null,
+                                        abbreviation varchar(10) NOT null,
+                                        type integer not null,
+                                        address text null,
+                                        phone varchar(20) null
+);
+
+
+
+
+CREATE TABLE admin (
+                       id integer PRIMARY KEY AUTO_INCREMENT,
+                       firstname varchar(32) NOT null,
+                       lastname varchar(32) NOT null,
+                       patronymic varchar(32) null,
+                       email varchar(256) UNIQUE NOT null,
+                       password varchar(20) NOT null,
+                       access_mode integer NOT null,
+                       icon text null,
+                       institution_id integer NOT null,
+
+                       FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                           ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE institution_event (
+                                   id integer PRIMARY KEY AUTO_INCREMENT,
+                                   title varchar(32) NOT null,
+                                   description text NOT null,
+                                   image text NOT null,
+                                   duration integer NOT null,
+                                   type integer NOT null,
+                                   institution_id integer NOT null,
+
+                                   FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                                       ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE event_service_info (
+                                    id integer PRIMARY KEY AUTO_INCREMENT,
+                                    date timestamp DEFAULT CURRENT_TIMESTAMP NOT null,
+                                    event_id integer UNIQUE NOT null,
+                                    admin_id integer null,
+
+                                    FOREIGN KEY (admin_id) REFERENCES admin(id)
+                                        ON UPDATE CASCADE ON DELETE CASCADE,
+
+                                    FOREIGN KEY (event_id) REFERENCES institution_event(id)
+                                        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE event_reward (
+                              id integer PRIMARY KEY AUTO_INCREMENT,
+                              public_hours double null,
+                              work_hours double null,
+                              event_id integer UNIQUE NOT null,
+
+                              FOREIGN KEY (event_id) REFERENCES institution_event(id)
+                                  ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE event_date (
+                            id integer PRIMARY KEY AUTO_INCREMENT,
+                            event_date timestamp null,
+                            start_time time null,
+                            meeting_time time null,
+                            event_id integer UNIQUE NOT null,
+
+                            FOREIGN KEY (event_id) REFERENCES institution_event(id)
+                                ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE event_place (
+                             id integer PRIMARY KEY AUTO_INCREMENT,
+                             address varchar(256) null,
+                             place varchar(256) null,
+                             event_id integer UNIQUE NOT null,
+
+                             FOREIGN KEY (event_id) REFERENCES institution_event(id)
+                                 ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE event_sorting (
+                               id integer PRIMARY KEY AUTO_INCREMENT,
+                               gender integer null,
+                               maximum_amount integer null,
+                               age_from integer null,
+                               age_to integer null,
+                               event_id integer UNIQUE NOT null,
+
+                               FOREIGN KEY (event_id) REFERENCES institution_event(id)
+                                   ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE practice (
+                          id integer PRIMARY KEY AUTO_INCREMENT,
+                          icon text null,
+                          payability boolean NOT null,
+                          description text NOT null,
+                          work_type integer NOT null,
+                          title varchar(32) NOT null,
+                          website text null,
+                          institution_id integer NOT null,
+
+                          FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                              ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE relocation (
+                            id integer PRIMARY KEY AUTO_INCREMENT,
+                            name varchar(32) NOT null,
+                            practice_id integer NOT null,
+
+                            FOREIGN KEY (practice_id) REFERENCES practice(id)
+                                ON UPDATE CASCADE ON DELETE CASCADE,
+
+                            UNIQUE(name, practice_id)
+);
+
+
+
+
+CREATE TABLE information_block (
+                                   id integer PRIMARY KEY AUTO_INCREMENT,
+                                   title varchar(32) NOT null,
+                                   content text NOT null,
+                                   practice_id integer NOT null,
+
+                                   FOREIGN KEY (practice_id) REFERENCES practice(id)
+                                       ON UPDATE CASCADE ON DELETE CASCADE,
+
+                                   UNIQUE(title, practice_id)
+);
+
+
+
+
+CREATE TABLE section (
+                         id integer PRIMARY KEY AUTO_INCREMENT,
+                         title varchar(32) NOT null,
+                         trainer varchar(128) NOT null,
+                         price boolean NOT null,
+                         gender integer NOT null,
+                         description text null,
+                         icon text null,
+                         from_course integer null,
+                         to_course integer null,
+                         institution_id integer NOT null,
+
+                         FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                             ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE pupil (
+                       id integer PRIMARY KEY AUTO_INCREMENT,
+                       email varchar(256) UNIQUE NOT null,
+                       password varchar(16) NOT null,
+                       icon text null,
+                       code varchar(16) NOT null,
+                       institution_id integer NOT null,
+
+                       FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                           ON UPDATE CASCADE ON DELETE CASCADE,
+
+                       UNIQUE(code, institution_id)
+);
+
+
+
+
+CREATE TABLE notification (
+                              id integer PRIMARY KEY AUTO_INCREMENT,
+                              icon text NOT null,
+                              title varchar(32) NOT null,
+                              description text null,
+                              date_time timestamp DEFAULT CURRENT_TIMESTAMP NOT null,
+                              status boolean DEFAULT false NOT null,
+                              pupil_id integer null,
+                              admin_id integer null,
+                              institution_id integer not null,
+
+                              FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                                  ON DELETE CASCADE ON UPDATE CASCADE,
+
+                              FOREIGN KEY (pupil_id) REFERENCES pupil(id)
+                                  ON DELETE CASCADE ON UPDATE CASCADE,
+
+                              FOREIGN KEY (admin_id) REFERENCES admin(id)
+                                  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE section_question (
+                                  id integer PRIMARY KEY AUTO_INCREMENT,
+                                  message text NOT null,
+                                  date timestamp DEFAULT CURRENT_TIMESTAMP NOT null,
+                                  status integer DEFAULT 1 NOT null,
+                                  pupil_id integer NOT null,
+                                  institution_id integer NOT null,
+
+                                  FOREIGN KEY (pupil_id) REFERENCES pupil(id)
+                                      ON DELETE CASCADE ON UPDATE CASCADE,
+
+                                  FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                                      ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE section_answer (
+                                id integer PRIMARY KEY AUTO_INCREMENT,
+                                message text NOT null,
+                                date timestamp DEFAULT CURRENT_TIMESTAMP NOT null,
+                                status integer DEFAULT 1 NOT null,
+                                question_id integer NOT null,
+
+                                FOREIGN KEY (question_id) REFERENCES section_question(id)
+                                    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE faculty (
+                         id integer PRIMARY KEY AUTO_INCREMENT,
+                         name varchar(256) NOT null,
+                         abbreviation varchar(10) NOT null,
+                         institution_id integer NOT null,
+
+                         FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                             ON DELETE CASCADE ON UPDATE CASCADE,
+
+                         UNIQUE(name, institution_id)
+);
+
+
+
+
+CREATE TABLE speciality (
+                            id integer PRIMARY KEY AUTO_INCREMENT,
+                            name varchar(256) NOT null,
+                            abbreviation varchar(10) NOT null,
+                            faculty_id integer NOT null,
+
+                            FOREIGN KEY (faculty_id) REFERENCES faculty(id)
+                                ON DELETE CASCADE ON UPDATE CASCADE,
+
+                            UNIQUE(name, faculty_id)
+);
+
+
+
+
+CREATE TABLE unconfirmed_pupil (
+                                   id integer PRIMARY KEY AUTO_INCREMENT,
+                                   code varchar(16) NOT null,
+                                   email varchar(256) UNIQUE NOT null,
+                                   password varchar(16) NOT null,
+                                   group_name varchar(32) NOT null,
+                                   institution_id integer NOT null,
+                                   speciality_id integer NOT null,
+                                   faculty_id integer NOT null,
+
+                                   FOREIGN KEY (institution_id) REFERENCES educational_intitution(id)
+                                       ON DELETE CASCADE ON UPDATE CASCADE,
+
+                                   FOREIGN KEY (faculty_id) REFERENCES faculty(id)
+                                       ON DELETE CASCADE ON UPDATE CASCADE,
+
+                                   FOREIGN KEY (speciality_id) REFERENCES speciality(id)
+                                       ON DELETE CASCADE ON UPDATE CASCADE,
+
+                                   UNIQUE(code, institution_id)
+);
+
+
+
+
+CREATE TABLE institution_group (
+                                   id integer PRIMARY KEY AUTO_INCREMENT,
+                                   name varchar(32) NOT null,
+                                   course integer NOT null,
+                                   speciality_id integer NOT null,
+
+                                   FOREIGN KEY (speciality_id) REFERENCES speciality(id)
+                                       ON DELETE CASCADE ON UPDATE CASCADE,
+
+                                   UNIQUE(name, speciality_id)
+);
+
+
+
+
+CREATE TABLE group_member (
+                              id integer PRIMARY KEY AUTO_INCREMENT,
+                              firstname varchar(32) NOT null,
+                              lastname varchar(32) NOT null,
+                              patronymic varchar(32) null,
+                              code varchar(16) NOT null,
+                              pupil_id integer null,
+                              group_id integer NOT null,
+
+                              FOREIGN KEY (pupil_id) REFERENCES pupil(id)
+                                  ON UPDATE CASCADE,
+
+                              FOREIGN KEY (group_id) REFERENCES institution_group(id)
+                                  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE exam (
+                      id integer PRIMARY KEY AUTO_INCREMENT,
+                      type integer NOT null,
+                      name varchar(32) NOT null,
+                      audience varchar(10) NOT null,
+                      date_time timestamp NOT null,
+                      status integer DEFAULT 0 NOT null,
+                      group_id integer NOT null,
+
+                      FOREIGN KEY (group_id) REFERENCES institution_group(id)
+                          ON DELETE CASCADE ON UPDATE CASCADE,
+
+                      UNIQUE(name, group_id)
+);
+
+
+
+
+CREATE TABLE schedule (
+                          id integer PRIMARY KEY AUTO_INCREMENT,
+                          name varchar(32) NOT null,
+                          finish_date timestamp NOT null,
+                          start_type integer NOT null,
+                          start_date timestamp DEFAULT CURRENT_TIMESTAMP NOT null,
+                          type integer DEFAULT 0 NOT null,
+                          group_id integer NOT null,
+
+                          FOREIGN KEY (group_id) REFERENCES institution_group(id)
+                              ON DELETE CASCADE ON UPDATE CASCADE,
+
+                          UNIQUE(name, group_id)
+);
+
+
+
+
+CREATE TABLE lesson (
+                        id integer PRIMARY KEY AUTO_INCREMENT,
+                        name varchar(64) NOT null,
+                        start time NOT null,
+                        end time NOT null,
+                        teacher varchar(128) NOT null,
+                        audience varchar(10) NOT null,
+                        type integer NOT null,
+                        dayofweek integer NOT null,
+                        week_type integer NOT null,
+                        schedule_id integer NOT null,
+
+                        FOREIGN KEY (schedule_id) REFERENCES schedule(id)
+                            ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE pass (
+                      id integer PRIMARY KEY AUTO_INCREMENT,
+                      date timestamp NOT null,
+                      status integer DEFAULT -1 NOT null,
+                      pupil_id integer NOT null,
+                      lesson_id integer NOT null,
+
+                      FOREIGN KEY (pupil_id) REFERENCES group_member(id)
+                          ON DELETE CASCADE ON UPDATE CASCADE,
+
+                      FOREIGN KEY (lesson_id) REFERENCES lesson(id)
+                          ON DELETE CASCADE ON UPDATE CASCADE,
+
+                      UNIQUE(date, lesson_id, pupil_id)
+);
+
+
+
+
+CREATE TABLE news (
+                      id integer PRIMARY KEY AUTO_INCREMENT,
+                      title varchar(128) NOT null,
+                      image text null,
+                      description text null
+);
+
+
+
+
+CREATE TABLE guide (
+                       id integer PRIMARY KEY AUTO_INCREMENT,
+                       title varchar(128) NOT null,
+                       description text null,
+                       image text null,
+                       news_id integer NOT null,
+
+                       FOREIGN KEY (news_id) REFERENCES news(id)
+                           ON DELETE CASCADE ON UPDATE CASCADE
+);
---
+++ src/main/resources/db/migration/V2__Triggers_for_database_initializing.sql
@@ -0,0 +1,148 @@
+DELIMITER $$
+
+CREATE TRIGGER notification_receiver_trigger
+    BEFORE INSERT ON notification
+    FOR EACH ROW
+BEGIN
+    IF (NEW.pupil_id IS NOT NULL AND NEW.admin_id IS NULL) OR
+       (NEW.pupil_id IS NULL AND NEW.admin_id IS NOT NULL) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Only one field should be set: either pupil_id or admin_id';
+END IF;
+END$$
+
+DELIMITER ;
+
+
+
+
+DELIMITER $$
+
+CREATE TRIGGER after_group_member_insert_trigger
+    AFTER INSERT ON group_member
+    FOR EACH ROW
+BEGIN
+    DECLARE pupil_id integer;
+    DECLARE unconfirmed_pupil_id integer;
+    DECLARE new_pupil_id integer;
+
+    SELECT id INTO pupil_id FROM pupil WHERE pupil.code = NEW.code LIMIT 1;
+
+    IF pupil_id IS NOT null THEN
+    UPDATE group_member SET group_member.pupil_id = pupil_id WHERE id = NEW.id;
+    ELSE
+    SELECT id INTO unconfirmed_pupil_id FROM unconfirmed_pupil WHERE unconfirmed_pupil.code = NEW.code LIMIT 1;
+
+    IF unconfirmed_pupil_id IS NOT null THEN
+    		INSERT IGNORE INTO pupil(email, password, institution_id, code)
+    SELECT email, password, institution_id, code FROM unconfirmed_pupil WHERE unconfirmed_pupil_id = id;
+
+    SELECT id INTO new_pupil_id FROM pupil WHERE email = (SELECT email FROM unconfirmed_pupil WHERE unconfirmed_pupil_id = id);
+
+    UPDATE group_member SET pupil_id = new_pupil_id WHERE id = NEW.id;
+END IF;
+END IF;
+END$$
+
+DELIMITER ;
+
+
+
+
+
+DELIMITER $$
+
+CREATE TRIGGER unconfirmed_pupil_deletion_after_registration
+    AFTER INSERT ON pupil
+    FOR EACH ROW
+BEGIN
+    DELETE FROM unconfirmed_pupil WHERE code = NEW.code;
+    END$$
+
+    DELIMITER ;
+
+
+
+
+
+
+DELIMITER $$
+
+    CREATE TRIGGER set_group_member_link_after_registration
+        AFTER INSERT ON pupil
+        FOR EACH ROW
+    BEGIN
+        DECLARE member_id integer;
+
+        SELECT id INTO member_id FROM group_member WHERE code = NEW.code;
+
+        IF member_id IS NOT null THEN
+        UPDATE group_member SET pupil_id = NEW.id;
+    END IF;
+    END$$
+
+    DELIMITER ;
+
+
+
+
+
+DELIMITER $$
+
+    CREATE TRIGGER unconfirmed_pupil_registration_check
+        BEFORE INSERT ON unconfirmed_pupil
+        FOR EACH ROW
+    BEGIN
+        DECLARE pupil_id integer;
+
+        SELECT id INTO pupil_id FROM pupil WHERE email = NEW.email;
+        IF pupil_id IS NOT null THEN
+    	SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'A user with this email is already registered!';
+        ELSE
+        SELECT id INTO pupil_id FROM pupil WHERE code = NEW.code AND institution_id = NEW.institution_id;
+        IF pupil_id IS NOT null THEN
+        	SIGNAL SQLSTATE '45000'
+        	SET MESSAGE_TEXT = 'The user in the selected educational institution with the specified code is already registered!';
+    END IF;
+END IF;
+END$$
+
+DELIMITER ;
+
+
+
+
+
+DELIMITER $$
+
+CREATE TRIGGER group_member_exists_check
+    BEFORE INSERT ON group_member
+    FOR EACH ROW
+BEGIN
+    DECLARE institution_id integer;
+    DECLARE group_member_id integer;
+
+    SELECT educational_intitution.id INTO institution_id FROM institution_group
+                                                                  JOIN speciality ON institution_group.speciality_id = speciality.id
+                                                                  JOIN faculty ON speciality.faculty_id = faculty.id
+                                                                  JOIN educational_intitution ON faculty.institution_id = educational_intitution.id
+    WHERE institution_group.id = NEW.group_id
+        LIMIT 1;
+
+    SELECT group_member.id INTO group_member_id FROM group_member
+                                                         JOIN institution_group ON group_member.group_id = institution_group.id
+                                                         JOIN speciality ON institution_group.speciality_id = speciality.id
+                                                         JOIN faculty ON speciality.faculty_id = faculty.id
+                                                         JOIN educational_intitution ON faculty.institution_id = educational_intitution.id
+    WHERE group_member.code = NEW.code
+      AND educational_intitution.id = institution_id
+        LIMIT 1;
+
+    IF group_member_id IS NOT null THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'A student with this code already exists in another group!';
+END IF;
+END$$
+
+DELIMITER ;
