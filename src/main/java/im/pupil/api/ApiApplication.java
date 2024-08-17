a182e28a289e7b4170ea612e6062d092227cc19b
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-08-18 01:07:32 +0300

    change main method to get mapper in application context

---
+++ src/main/java/im/pupil/api/ApiApplication.java
@@ -0,0 +1,26 @@
+package im.pupil.api;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+				.setMatchingStrategy(MatchingStrategies.STRICT)
+				.setFieldMatchingEnabled(true)
+				.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+				.setAmbiguityIgnored(true);
+		return modelMapper;
+	}
+}
