6126b5ef2e61c86b471037bc66b9f597c4f286a6
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-08-18 00:18:17 +0300

    create dto for educational_institution endtity and institution_entity

---
+++ src/main/java/im/pupil/api/dto/InstitutionEventDto.java
@@ -0,0 +1,83 @@
+package im.pupil.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * DTO for {@link im.pupil.api.model.InstitutionEvent}
+ */
+public class InstitutionEventDto implements Serializable {
+    @NotNull
+    @Size(max = 32)
+    @NotEmpty(message = "Title should be not empty")
+    @NotBlank(message = "Title should be not blank")
+    private final String title;
+    @NotNull
+    private final String description;
+    @NotNull
+    private final String image;
+    @NotNull
+    private final Integer duration;
+    @NotNull
+    private final Integer type;
+
+    public InstitutionEventDto(String title, String description, String image, Integer duration, Integer type) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.duration = duration;
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InstitutionEventDto entity = (InstitutionEventDto) o;
+        return Objects.equals(this.title, entity.title) &&
+                Objects.equals(this.description, entity.description) &&
+                Objects.equals(this.image, entity.image) &&
+                Objects.equals(this.duration, entity.duration) &&
+                Objects.equals(this.type, entity.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, image, duration, type);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "title = " + title + ", " +
+                "description = " + description + ", " +
+                "image = " + image + ", " +
+                "duration = " + duration + ", " +
+                "type = " + type + ")";
+    }
+}
