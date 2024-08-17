b7ec1ac8a4291f56346d69b61397faa6dfc342a2
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-08-18 01:06:40 +0300

    add controler with endpoint to get institutional events

---
+++ src/main/java/im/pupil/api/controller/EducationInstitutionController.java
@@ -0,0 +1,48 @@
+package im.pupil.api.controller;
+
+import im.pupil.api.dto.InstitutionEventDto;
+import im.pupil.api.service.EducationInstitutionService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/education/institution")
+public class EducationInstitutionController {
+    private final EducationInstitutionService educationInstitutionService;
+
+    @Autowired
+    public EducationInstitutionController(EducationInstitutionService educationInstitutionService, ModelMapper modelMapper) {
+        this.educationInstitutionService = educationInstitutionService;
+    }
+
+    @GetMapping("/events/id/{institutionId}")
+    public List<InstitutionEventDto> getEventsByInstitutionId(@PathVariable Integer institutionId) {
+        return educationInstitutionService
+                .getInstitutionEventsOfEducationInstitutionById(institutionId)
+                .stream()
+                .map(educationInstitutionService::convertInstitutionEventToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/events/institutionName/{institutionName}")
+    public List<InstitutionEventDto> getEventsByInstitutionName(@PathVariable String institutionName) {
+        return educationInstitutionService
+                .getInstitutionEventsOfEducationInstitutionByName(institutionName)
+                .stream()
+                .map(educationInstitutionService::convertInstitutionEventToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/events/instituteAbbreviation/{institutionAbbreviation}")
+    public List<InstitutionEventDto> getEventsByInstitutionAbbreviation(@PathVariable String institutionAbbreviation) {
+        return educationInstitutionService
+                .getInstitutionEventsOfEducationInstitutionByAbbreviation(institutionAbbreviation)
+                .stream()
+                .map(educationInstitutionService::convertInstitutionEventToDto)
+                .collect(Collectors.toList());
+    }
+}
