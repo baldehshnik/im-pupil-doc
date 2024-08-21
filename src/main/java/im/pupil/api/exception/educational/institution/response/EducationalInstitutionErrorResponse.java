ae996e242f0bb37876f4875c1be3052d267cf075
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-08-21 23:01:45 +0300

    create error responses for InstitutionEvent and EducationalInstitution

---
+++ src/main/java/im/pupil/api/exception/educational/institution/response/EducationalInstitutionErrorResponse.java
@@ -0,0 +1,10 @@
+package im.pupil.api.exception.educational.institution.response;
+
+import im.pupil.api.exception.ErrorResponse;
+
+public class EducationalInstitutionErrorResponse extends ErrorResponse {
+
+    public EducationalInstitutionErrorResponse(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/im/pupil/api/exception/insitution/event/response/InstitutionEventErrorResponse.java
@@ -0,0 +1,9 @@
+package im.pupil.api.exception.insitution.event.response;
+
+import im.pupil.api.exception.ErrorResponse;
+
+public class InstitutionEventErrorResponse extends ErrorResponse {
+    public InstitutionEventErrorResponse(String message) {
+        super(message);
+    }
+}
