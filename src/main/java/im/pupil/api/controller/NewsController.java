4e369171060c2087b8881f0f18e75b6a564887c1
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-08-24 16:18:03 +0300

    add exception handling for NewsController

--- src/main/java/im/pupil/api/controller/NewsController.java
+++ src/main/java/im/pupil/api/controller/NewsController.java
@@ -1,8 +1,13 @@
 package im.pupil.api.controller;
 
 import im.pupil.api.dto.NewsDto;
+import im.pupil.api.exception.news.NewsNotFoundException;
+import im.pupil.api.exception.news.response.NewsErrorResponse;
 import im.pupil.api.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +32,12 @@ public class NewsController {
                 .stream()
                 .map(newsService::convertToDto)
                 .collect(Collectors.toList());
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<NewsErrorResponse> handleNotFoundException(NewsNotFoundException exception) {
+        NewsErrorResponse newsErrorResponse = new NewsErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(newsErrorResponse, HttpStatus.NOT_FOUND);
     }
 }
