60220d8c94c058ac89774057027c9d958af91664
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-03 01:43:31 +0300

    create method for admin finding in specific education institution. extract all available dto conversions into nested class

--- src/main/java/im/pupil/api/service/EducationalInstitutionService.java
+++ src/main/java/im/pupil/api/service/EducationalInstitutionService.java
@@ -1,10 +1,14 @@
 package im.pupil.api.service;
 
+import im.pupil.api.dto.AdminDto;
 import im.pupil.api.dto.InstitutionEventDto;
+import im.pupil.api.exception.admin.AdminNotFoundException;
 import im.pupil.api.exception.educational.institution.EducationalInstitutionNotFoundException;
 import im.pupil.api.exception.insitution.event.InstitutionEventNotFoundException;
+import im.pupil.api.model.Admin;
 import im.pupil.api.model.EducationalInstitution;
 import im.pupil.api.model.InstitutionEvent;
+import im.pupil.api.repository.AdminRepository;
 import im.pupil.api.repository.EducationalInstitutionRepository;
 import im.pupil.api.repository.InstitutionEventRepository;
 import org.modelmapper.ModelMapper;
@@ -19,16 +23,22 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class EducationalInstitutionService {
 
-    EducationalInstitutionRepository educationalInstitutionRepository;
-    InstitutionEventRepository institutionEventRepository;
-    ModelMapper modelMapper;
+    final EducationalInstitutionRepository educationalInstitutionRepository;
+    final InstitutionEventRepository institutionEventRepository;
+    final AdminRepository adminRepository;
+    final AdminService adminService;
+    final ModelMapper modelMapper;
 
     @Autowired
     public EducationalInstitutionService(EducationalInstitutionRepository educationalInstitutionRepository,
                                          InstitutionEventRepository institutionEventRepository,
+                                         AdminRepository adminRepository,
+                                         AdminService adminService,
                                          ModelMapper modelMapper) {
         this.educationalInstitutionRepository = educationalInstitutionRepository;
         this.institutionEventRepository = institutionEventRepository;
+        this.adminRepository = adminRepository;
+        this.adminService = adminService;
         this.modelMapper = modelMapper;
     }
 
@@ -69,6 +79,17 @@ public class EducationalInstitutionService {
         return result;
     }
 
+    public List<Admin> getAdminsOfEducationalInstitutionByInstitutionId(Integer institutionId) {
+        EducationalInstitution educationalInstitution = findEducationalInstitutionById(institutionId);
+        List<Admin> admins = adminRepository.findByInstitution_Id(educationalInstitution.getId());
+
+        if (admins.isEmpty()) {
+            throw new AdminNotFoundException("No admin found with institution id " + institutionId);
+        }
+
+        return admins;
+    }
+
     private EducationalInstitution findEducationalInstitutionById(Integer institutionId) {
         Optional<EducationalInstitution> educationalInstitution = educationalInstitutionRepository.findById(institutionId);
         return educationalInstitution.orElseThrow(() -> new EducationalInstitutionNotFoundException("No educational institution found with id: " + institutionId));
@@ -84,11 +105,27 @@ public class EducationalInstitutionService {
         return educationalInstitution.orElseThrow(() -> new EducationalInstitutionNotFoundException("No educational institution found with abbreviation: " + institutionAbbreviation));
     }
 
-    public InstitutionEventDto convertToDto(InstitutionEvent institutionEvent) {
-        return modelMapper.map(institutionEvent, InstitutionEventDto.class);
+    public EducationInstitutionAssociatedConverters getEducationInstitutionAssociatedConverters() {
+        return new EducationInstitutionAssociatedConverters();
     }
 
-    public InstitutionEvent convertToEvent(InstitutionEventDto institutionEventDto) {
-        return modelMapper.map(institutionEventDto, InstitutionEvent.class);
+    public class EducationInstitutionAssociatedConverters {
+        public EducationInstitutionAssociatedConverters() {}
+
+        public InstitutionEventDto convertInstitutionEventToDto(InstitutionEvent institutionEvent) {
+            return modelMapper.map(institutionEvent, InstitutionEventDto.class);
+        }
+
+        public InstitutionEvent convertInstitutionEventDtoToEntity(InstitutionEventDto institutionEventDto) {
+            return modelMapper.map(institutionEventDto, InstitutionEvent.class);
+        }
+
+        public AdminDto convertAdminToDto(Admin admin) {
+            return adminService.convertToDto(admin);
+        }
+
+        public Admin convertAdminDtoToEntity(AdminDto adminDto) {
+            return adminService.convertToEntity(adminDto);
+        }
     }
 }
