74c5c0c3bc577e1f23d5898e50b05ce45637118e
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-03 22:14:14 +0300

    remove import

--- src/main/java/im/pupil/api/controller/AdminController.java
+++ src/main/java/im/pupil/api/controller/AdminController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
