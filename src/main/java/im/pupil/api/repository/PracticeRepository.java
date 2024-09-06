4e81fe6e8dc65f8e84939cd8fc527e870c2f90a3
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-06 16:26:36 +0300

    add PracticeRepository

---
+++ src/main/java/im/pupil/api/repository/PracticeRepository.java
@@ -0,0 +1,13 @@
+package im.pupil.api.repository;
+
+import im.pupil.api.model.Practice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PracticeRepository extends JpaRepository<Practice, Integer> {
+    Optional<Practice> findPracticeById(Integer id);
+    List<Practice> findByInstitution_Id(Integer id);
+}
