49106c47c64988de2ee87dd261bd6f0deea382e0
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-06 16:26:27 +0300

    add PracticeNotFoundException, PracticeErrorResponse

---
+++ src/main/java/im/pupil/api/exception/practice/response/PracticeErrorResponse.java
@@ -0,0 +1,14 @@
+package im.pupil.api.exception.practice.response;
+
+import im.pupil.api.exception.ErrorResponse;
+
+public class PracticeErrorResponse extends ErrorResponse {
+    public PracticeErrorResponse(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
