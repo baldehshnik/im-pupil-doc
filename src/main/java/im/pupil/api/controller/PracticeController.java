28d48e626db6e8d74ee0a5196cc553657d3bbd6d
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-08 19:13:00 +0300

    add mapping for Practice registration, make methods for exception handling

--- src/main/java/im/pupil/api/controller/PracticeController.java
+++ src/main/java/im/pupil/api/controller/PracticeController.java
@@ -3,6 +3,7 @@ package im.pupil.api.controller;
 import im.pupil.api.dto.PracticeDto;
 import im.pupil.api.exception.educational.institution.EducationalInstitutionNotFoundException;
 import im.pupil.api.exception.educational.institution.response.EducationalInstitutionErrorResponse;
+import im.pupil.api.exception.practice.PracticeNotCreatedException;
 import im.pupil.api.exception.practice.PracticeNotFoundException;
 import im.pupil.api.exception.practice.response.PracticeErrorResponse;
 import im.pupil.api.service.PracticeService;
@@ -10,9 +11,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -84,6 +88,19 @@ public class PracticeController {
                 .collect(Collectors.toList());
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<HttpStatus> performPractiseRegistration(@RequestBody @Valid PracticeDto practiceDto,
+                                                                  BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new PracticeNotCreatedException(buildErrorMessageByBindingResult(bindingResult));
+        }
+
+        practiceService.createPracticeWithRelocationAndEducationInstitutionAndInformationBlock(practiceService.convertToEntity(practiceDto));
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
     @ExceptionHandler
     private ResponseEntity<PracticeErrorResponse> handlePracticeNotFoundException(PracticeNotFoundException exception) {
         PracticeErrorResponse practiceErrorResponse = new PracticeErrorResponse(exception.getMessage());
@@ -97,5 +114,33 @@ public class PracticeController {
         EducationalInstitutionErrorResponse errorResponse = new EducationalInstitutionErrorResponse(exception.getMessage());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.FAILED_DEPENDENCY);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<PracticeErrorResponse> handlePracticeNotCreatedException (
+            PracticeNotCreatedException exception){
+        PracticeErrorResponse errorResponse = new PracticeErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<PracticeErrorResponse> handleConstraintViolationException (
+            ConstraintViolationException exception){
+        PracticeErrorResponse errorResponse = new PracticeErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    private String buildErrorMessageByBindingResult(BindingResult bindingResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        bindingResult.getFieldErrors().forEach(
+                (errorField) -> stringBuilder
+                        .append(errorField.getField())
+                        .append(" - ")
+                        .append(errorField.getDefaultMessage())
+                        .append("; ")
+        );
+        return stringBuilder.toString();
     }
 }
