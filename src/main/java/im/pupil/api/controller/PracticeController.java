c623dc2b070661f3d5ecec50f7b03dfd5f2a88c5
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-08 19:41:47 +0300

    add description for swagger, add new exception handlers, change response code for handling ConstraintViolationException

--- src/main/java/im/pupil/api/controller/PracticeController.java
+++ src/main/java/im/pupil/api/controller/PracticeController.java
@@ -3,9 +3,13 @@ package im.pupil.api.controller;
 import im.pupil.api.dto.PracticeDto;
 import im.pupil.api.exception.educational.institution.EducationalInstitutionNotFoundException;
 import im.pupil.api.exception.educational.institution.response.EducationalInstitutionErrorResponse;
+import im.pupil.api.exception.information.block.InformationBlockAlreadyExistsException;
+import im.pupil.api.exception.information.block.response.InformationBlockErrorResponse;
 import im.pupil.api.exception.practice.PracticeNotCreatedException;
 import im.pupil.api.exception.practice.PracticeNotFoundException;
 import im.pupil.api.exception.practice.response.PracticeErrorResponse;
+import im.pupil.api.exception.relocation.RelocationAlreadyExistsException;
+import im.pupil.api.exception.relocation.response.RelocationErrorResponse;
 import im.pupil.api.service.PracticeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -88,6 +92,58 @@ public class PracticeController {
                 .collect(Collectors.toList());
     }
 
+    @Operation(summary = "Create new practice",
+            description = "Creates new practice with new relocation and new description block. " +
+                    "If any of the records does not meet the uniqueness conditions, " +
+                    "than AlreadyExistsException will be thrown")
+    @ApiResponse(
+            responseCode = "201",
+            description = "Create new practice",
+            content = { @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PracticeDto.class))
+            }
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Practice not created",
+            content = { @Content (
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PracticeErrorResponse.class))
+            }
+    )
+    @ApiResponse(
+            responseCode = "406",
+            description = "Practice constraints violated",
+            content = { @Content (
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PracticeErrorResponse.class))
+            }
+    )
+    @ApiResponse(
+            responseCode = "409",
+            description = "Relocation already exists",
+            content = { @Content (
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PracticeErrorResponse.class))
+            }
+    )
+    @ApiResponse(
+            responseCode = "409",
+            description = "Information block already exists",
+            content = { @Content (
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PracticeErrorResponse.class))
+            }
+    )
+    @ApiResponse(
+            responseCode = "424",
+            description = "Education institution, which is used for practice finding, not existing",
+            content = { @Content (
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = EducationalInstitutionErrorResponse.class))
+            }
+    )
     @PostMapping("/register")
     public ResponseEntity<HttpStatus> performPractiseRegistration(@RequestBody @Valid PracticeDto practiceDto,
                                                                   BindingResult bindingResult) {
@@ -129,7 +185,23 @@ public class PracticeController {
             ConstraintViolationException exception){
         PracticeErrorResponse errorResponse = new PracticeErrorResponse(exception.getMessage());
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<RelocationErrorResponse> handleRelocationAlreadyExistsException (
+            RelocationAlreadyExistsException exception){
+        RelocationErrorResponse errorResponse = new RelocationErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<InformationBlockErrorResponse> handleRelocationAlreadyExistsException (
+            InformationBlockAlreadyExistsException exception){
+        InformationBlockErrorResponse errorResponse = new InformationBlockErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     private String buildErrorMessageByBindingResult(BindingResult bindingResult) {
