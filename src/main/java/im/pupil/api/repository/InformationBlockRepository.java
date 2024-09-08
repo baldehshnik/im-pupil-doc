a7dd4c9f173c5eaa8d5c08bb56e5c32c13513f5d
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-08 18:56:06 +0300

    add InformationBlockRepository

--- src/main/java/im/pupil/api/repository/InformationBlockRepository.java
+++ src/main/java/im/pupil/api/repository/InformationBlockRepository.java
@@ -3,5 +3,9 @@ package im.pupil.api.repository;
 import im.pupil.api.model.InformationBlock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InformationBlockRepository extends JpaRepository<InformationBlock, Integer> {
+    Optional<InformationBlock> findInformationBlockById(Integer id);
+    Optional<InformationBlock> findInformationBlockByTitleAndPractice_Id(String name, Integer practiceId);
 }
