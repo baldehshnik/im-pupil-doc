615e7a4815198267dceb6f7b2a148b8d4c6e216c
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-08 18:57:44 +0300

    add RelocationRepository

--- src/main/java/im/pupil/api/repository/RelocationRepository.java
+++ src/main/java/im/pupil/api/repository/RelocationRepository.java
@@ -3,5 +3,9 @@ package im.pupil.api.repository;
 import im.pupil.api.model.Relocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RelocationRepository extends JpaRepository<Relocation, Integer> {
+    Optional<Relocation> findById(int id);
+    Optional<Relocation> findByNameAndPractice_Id(String name, int practiceId);
 }
