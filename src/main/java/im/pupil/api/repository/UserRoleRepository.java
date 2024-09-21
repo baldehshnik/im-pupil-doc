c485c29fb6056ccd6e2db459b2e2b56948aea448
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-21 13:36:01 +0300

    Add UerRepository, UserRoleRepository, RoleRepository

---
+++ src/main/java/im/pupil/api/repository/UserRoleRepository.java
@@ -0,0 +1,18 @@
+package im.pupil.api.repository;
+
+import im.pupil.api.model.Role;
+import im.pupil.api.model.UserRole;
+import im.pupil.api.model.UserRoleId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
+    @Query("SELECT ur.role FROM UserRole ur WHERE ur.user.id = :userId")
+    List<Role> findRolesByUserId(@Param("userId") Integer userId);
+
+    @Query("SELECT COUNT(ur) > 0 FROM UserRole ur WHERE ur.user.id = :userId AND ur.role.id = :roleId")
+    boolean existsByUserIdAndRoleId(@Param("userId") Integer userId, @Param("roleId") Integer roleId);
+}
