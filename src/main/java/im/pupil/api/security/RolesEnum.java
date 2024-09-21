3064ff0a0ae33f5103cca42e50fc514e92bf06dc
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-21 13:40:05 +0300

    Add RolesEnum

---
+++ src/main/java/im/pupil/api/security/RolesEnum.java
@@ -0,0 +1,16 @@
+package im.pupil.api.security;
+
+public enum RolesEnum {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private final String description;
+
+    RolesEnum(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
