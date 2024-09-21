7405daec4b07f434b4a2c981a8a3f945f433a386
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-09-21 13:32:41 +0300

    add table for users and roles

---
+++ src/main/java/im/pupil/api/model/User.java
@@ -0,0 +1,62 @@
+package im.pupil.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user", schema = "im_pupil", uniqueConstraints = {
+        @UniqueConstraint(name = "email", columnNames = {"email"})
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Size(max = 128)
+    @NotNull
+    @Column(name = "password", nullable = false, length = 128)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public Set<UserRole> getUserRoles() { return userRoles; }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
