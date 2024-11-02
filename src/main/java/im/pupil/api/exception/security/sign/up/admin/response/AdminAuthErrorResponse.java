eb714921278e56b70a5fd3b0aa1aeb4b5b00ae34
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-11-03 01:00:46 +0300

    add AdminAuthErrorResponse, RefreshTokenErrorResponse

---
+++ src/main/java/im/pupil/api/exception/security/sign/up/admin/response/AdminAuthErrorResponse.java
@@ -0,0 +1,14 @@
+package im.pupil.api.exception.security.sign.up.admin.response;
+
+import im.pupil.api.exception.ErrorResponse;
+
+public class AdminAuthErrorResponse extends ErrorResponse {
+    public AdminAuthErrorResponse(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
