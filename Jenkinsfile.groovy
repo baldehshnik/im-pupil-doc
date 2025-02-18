80a4b68df3b97c9b60dd8d2eadf8152b09fb12ce
Vladislav Shcherba <v.shcherba.dev@gmail.com>
2025-02-18 10:38:39 +0300

    environments were added to the project

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,23 @@
 pipeline {
     agent any
 
+    environment {
+        DATABASE_URL = credentials('database-url')
+        DATABASE_USER = credentials('database-user')
+        DATABASE_PASSWORD = credentials('database-password')
+        SERVER_URL = credentials('server-url')
+        SERVER_PORT = credentials('server-port')
+        REFRESH_TOKEN_EXPIRATION_DATE = credentials('refresh-token-expiration-date')
+        JSON_TOKEN_SIGNING_KEY = credentials('json-token-signing-key')
+        S3_ENDPOINT_URL = credentials('s3-endpoint-url')
+        S3_ACCESS_KEY = credentials('s3-access-key')
+        S3_BUCKET_NAME = credentials('s3-bucket-name')
+    }
+
     stages {
         stage('Build') {
             steps {
-                script {
-                    env.LOAD_DOTENV = true
-                    load '${WORKSPACE}/.env'
-
-                    sh './mvnw clean spring-boot:run'
-                }
+                sh './mvnw clean spring-boot:run'
             }
         }
     }
