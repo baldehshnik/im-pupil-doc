1904ce61a5ad7b3cefafa6a6e28a314e044ff975
Vladislav Shcherba <v.shcherba.dev@gmail.com>
2025-02-21 10:22:12 +0300

    email notification was enabled after successful and failed pipeline execution

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,6 +12,7 @@ pipeline {
         S3_ENDPOINT_URL = credentials('s3-endpoint-url')
         S3_ACCESS_KEY = credentials('s3-access-key')
         S3_BUCKET_NAME = credentials('s3-bucket-name')
+        JENKINS_ADMIN_EMAIL = credentials('jenkins-admin-email')
     }
 
     stages {
@@ -20,6 +21,23 @@ pipeline {
                 sh 'chmod +x ./mvnw'
                 sh './mvnw clean compile'
             }
+        }
+    }
+
+    post {
+        success {
+            emailext(
+                    to: "${env.JENKINS_ADMIN_EMAIL}",
+                    subject: "Build Succeeded: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+                    body: "The build was successful! Please check the results.\n\nBuild link: ${env.BUILD_URL}"
+            )
+        }
+        failure {
+            emailext(
+                    to: "${env.JENKINS_ADMIN_EMAIL}",
+                    subject: "Build Failure: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+                    body: "The build has failed. Please take a look.\n\nBuild link: ${env.BUILD_URL}"
+            )
         }
     }
 }
